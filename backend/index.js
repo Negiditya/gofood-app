5c60ac3d7dca9e9e21de54ae4fdc3d622aac91d7
Negiditya <anegi1509@gmai.com>
2026-01-12 11:27:20 +0530

    Updated code

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,8 @@ const port = process.env.PORT || 5000;
 const cors = require("cors")
 app.use(express.json())
 require('dotenv').config({ debug: true });
-const mongoDB = require('./db');
 const verifyToken = require('./middleware/auth');
-mongoDB()
+
 
 
 
@@ -24,7 +23,7 @@ app.use(cors({
 
 
 app.use("/api", require("./Routes/AuthUser"))
-app.use("/api", verifyToken, require("./Routes/DisplayData"))
+app.use("/api", require("./Routes/DisplayData"))
 app.use("/api", verifyToken, require("./Routes/ordersData"))
 
 
